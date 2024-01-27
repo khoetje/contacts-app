6ba7272487264ee6334f0f84a0f4005c69b7ebf8
Gaby <gaby.khoe@gmail.com>
2024-01-27 23:01:29 +0100

    added a header with logo, title and intro text

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Contacts</h1>
+    <p>
+      Je kunt hier eenvoudig personen beheren en inzien in deze contactenlijst.
+    </p>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Home</h1>
+    <p>
+      Je kunt hier eenvoudig personen beheren en inzien in deze contactenlijst.
+    </p>
   </div>
 </template>
 
