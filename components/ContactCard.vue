74e39a30689cbf3cbd5b4146a7c64991dc75c66f
Gaby <gaby.khoe@gmail.com>
2024-01-28 22:42:39 +0100

    prepare components for contact list and contact card

---
+++ components/ContactCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contact-card">
+    {{ contact.name }}
+  </div>
+</template>
+
+<script>
+import ContactCard from './ContactCard.vue';
+export default {
+  props: ['contact'],
+  components: { ContactCard },
+  setup(props) {
+    console.log('props', props.contacts);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/ContactList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contact-list">
+    <div v-for="contact in contacts" :key="contact">
+      <ContactCard :contact="contact" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactCard from './ContactCard.vue';
+export default {
+  props: ['contacts'],
+  components: { ContactCard },
+  setup(props) {
+    console.log('props', props.contacts);
+  },
+};
+</script>
