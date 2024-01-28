0d290746da58dfa91d281cf12ea876d61b9c6555
Gaby <gaby.khoe@gmail.com>
2024-01-28 22:40:21 +0100

    made a component to show page header, renamed contacts page

---
+++ components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <h1 class="text-2xl font-bold my-8">{{ title }}</h1>
+    <p class="my-8">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'text'],
+  setup(props) {
+    // console.log('props', props.title);
+    // console.log('props', props.text);
+  },
+};
+</script>
---
+++ pages/contacten.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="text-center">
+      <PageHeader
+        title="Contacten"
+        text="Je kunt hier eenvoudig personen beheren en inzien in deze
+        contactenlijst."
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import PageHeader from '~/components/PageHeader.vue';
+export default {
+  components: { PageHeader },
+};
+</script>
