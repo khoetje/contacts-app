7af052fc0365a26c1d8afbc31957a6f125694604
Gaby <gaby.khoe@gmail.com>
2024-01-28 22:47:03 +0100

    Update to correct fields in store

--- store/form.js
+++ store/form.js
@@ -3,10 +3,10 @@ import { defineStore } from 'pinia';
 export const useFormStore = defineStore('form', {
   state: () => ({
     formData: {
-      // Define your form fields here
-      firstName: '',
-      lastName: '',
+      workPrivate: '',
+      fullName: '',
       email: '',
+      phone: '',
     },
   }),
 
