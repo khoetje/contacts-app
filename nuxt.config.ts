4295a3b55c55cc62c33dc79fdf83aa9a9fa08640
Gaby <gaby.khoe@gmail.com>
2024-01-28 22:38:32 +0100

    installed modules vee-validate and pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@vee-validate/nuxt',
+    '@pinia/nuxt',
+  ],
+  plugins: ['~/plugins/vee-validate.js'],
+  build: {
+    transpile: ['vee-validate/dist/rules'],
+  },
 });
