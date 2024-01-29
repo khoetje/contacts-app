14ef57f1c8e06af58d901f3163d9c5c223774c21
Gaby <gaby.khoe@gmail.com>
2024-01-29 22:37:14 +0100

    save state as an array in pinia store, save data in localStorage, and serialize sensitive data

--- store/form.js
+++ store/form.js
@@ -1,18 +1,40 @@
 import { defineStore } from 'pinia';
+import CryptoJS from 'crypto-js';
+
+export const STORAGE_KEY = 'sensitiveFormData';
 
 export const useFormStore = defineStore('form', {
   state: () => ({
-    formData: {
-      workPrivate: '',
-      fullName: '',
-      email: '',
-      phone: '',
-    },
+    formDataArray: [],
   }),
-
   actions: {
     setFormData(formData) {
-      this.formData = formData;
+      console.log('setFormData');
+      this.formDataArray.push(formData);
+      console.log('this.formDataArray', this.formDataArray);
+      this.setStorage(STORAGE_KEY, this.formDataArray);
+    },
+
+    getStorage: (key) => {
+      const encryptedData = localStorage.getItem(key);
+      if (encryptedData) {
+        const decryptedData = CryptoJS.AES.decrypt(
+          encryptedData,
+          'secret-123'
+        ).toString(CryptoJS.enc.Utf8);
+        return JSON.parse(decryptedData);
+      }
+      return {};
+    },
+
+    setStorage: (key, state) => {
+      const encryptedData = CryptoJS.AES.encrypt(
+        JSON.stringify(state),
+        'secret-123'
+      ).toString();
+
+      console.log('encryptedData', encryptedData);
+      localStorage.setItem(key, encryptedData);
     },
   },
 });
