45336fd3a787eb1d6d4b1ad4a703b82a8433200c
Gaby <gaby.khoe@gmail.com>
2024-01-29 22:37:54 +0100

    get state and use to render contact list

--- pages/contacten.vue
+++ pages/contacten.vue
@@ -5,12 +5,24 @@
       text="Je kunt hier eenvoudig personen beheren en inzien in deze
         contactenlijst."
     />
+    <ContactList :contacts="contacts" />
   </div>
 </template>
 
 <script>
+import { useFormStore, STORAGE_KEY } from '~/store/form';
 import PageHeader from '~/components/PageHeader.vue';
+import ContactList from '~/components/ContactList.vue';
+
 export default {
-  components: { PageHeader },
+  components: { PageHeader, ContactList },
+  setup() {
+    const formStore = useFormStore();
+    const contacts = formStore.getStorage(STORAGE_KEY);
+    return {
+      formStore,
+      contacts,
+    };
+  },
 };
 </script>
