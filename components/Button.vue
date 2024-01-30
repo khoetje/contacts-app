a1f6303253cc42ea877829c2b3fdef5f70a40462
Gaby <gaby.khoe@gmail.com>
2024-01-30 16:41:23 +0100

    make button align centered

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="my-8 px-5 py-2 bg-[#004df5] text-white rounded-[100px]"
+    class="my-8 px-5 py-2 bg-[#004df5] text-white rounded-[100px] flex inline-block m-auto"
     :type="type"
   >
     {{ label }}
