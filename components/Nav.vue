756305ff2747faefec5345cb399ae931c11555f5
Gaby <gaby.khoe@gmail.com>
2024-01-30 19:50:04 +0100

    added container for navigation

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav>
-    <ul>
+  <nav class="text-white lg:max-w-6xl pd-4 md:pd-8 xl:mx-auto">
+    <ul class="flex align-center justify-center p-4">
       <slot></slot>
     </ul>
   </nav>
