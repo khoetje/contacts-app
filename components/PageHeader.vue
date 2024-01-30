ecb123c29e30f3bc4c1035e5abce3a36dff47da8
Gaby <gaby.khoe@gmail.com>
2024-01-30 19:49:44 +0100

    define types for pageHeader props

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="header">
-    <h1 class="text-2xl font-bold my-8">{{ title }}</h1>
-    <p class="my-8">{{ text }}</p>
+  <div class="header w-4/5 md:w-3/5 m-auto">
+    <h1 class="text-3xl font-bold mt-8 mb-2">{{ title }}</h1>
+    <p class="mb-12 mt-2">{{ text }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['title', 'text'],
-  setup(props) {
-    // console.log('props', props.title);
-    // console.log('props', props.text);
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: false,
+    },
   },
 };
 </script>
